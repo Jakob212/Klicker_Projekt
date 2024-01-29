8f759f4fcea6cbcf2960b0e1017a358829c9dd66
Jakob212 <jakob.goetz555@gmail.com>
2024-01-29 17:01:40 +0100

    tkinter aufbau

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#hallo
+import tkinter as tk
+
+def start_recording():
+
+    pass
+
+def stop_recording():
+
+    pass
+
+def play_events():
+
+    pass
+
+app = tk.Tk()
+app.title("Ereignisaufzeichner")
+
+start_button = tk.Button(app, text="Start Aufnahme", command=start_recording)
+start_button.pack()
+
+stop_button = tk.Button(app, text="Stop Aufnahme", command=stop_recording)
+stop_button.pack()
+
+play_button = tk.Button(app, text="Ereignisse abspielen", command=play_events)
+play_button.pack()
+
+app.mainloop()
